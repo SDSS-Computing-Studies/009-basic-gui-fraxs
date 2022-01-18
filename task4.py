4d80d344554c99359784e2efdf6608f0df3c202b
fraxs <90715521+fraxs@users.noreply.github.com>
2022-01-18 13:53:16 -0800

    done

--- task4.py
+++ task4.py
@@ -1,16 +1,13 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk
 window = tk.Tk()
 window.title("Example")
-dogphoto = PhotoImage(file="dog.png")
+dogphoto = tk.PhotoImage(file="dog.png")
 Labeldogphoto = tk.Label(window,image=dogphoto)
 label1 = tk.Label(window,text="Pochacco!")
 label2 = tk.Label(window,text="A cuddly little puppy! This is from the same \ncreators who brought you Keropi and Kero Kero",background='#A3FFFF')
 
-
-Labeldogphoto.grid(row = 1, column = 1, rowspan = 3, sticky=E)
-
-label1.grid(row = 2, column = 2, sticky=W)
-label2.grid(row = 5, column =1,columnspan = 2,)
+Labeldogphoto.place(x = 60, y = 0)
+label1.place(x = 130, y = 50)
+label2.place(x = 0, y = 100)
+window.geometry("260x150")
 window.mainloop()
